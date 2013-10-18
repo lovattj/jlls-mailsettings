376cdaf91bc2ab12ebcb2614ffd299fc1a1f64c0
Jonathan <lovattj@me.com>
2013-10-18 07:14:16 +0100

    Throws HTTP 500 error if attempting to access unimplemented API method.

--- api.php
+++ api.php
@@ -44,10 +44,17 @@ default:
 }
 
 $result = Array('result' => $arraytoreturn);
+
 if (($arraytoreturn) == null) {
+	// Return 404 if a matching result wasn't found.
 	header("HTTP/1.1 404 File Not Found");
 	echo (json_encode($arraytoreturn));
+} else if ($arraytoreturn['Error'] == 'No method specified') {
+	// Return 500 if the user attempts to access an unimplemented API method.
+	header("HTTP/1.0 500 Internal Server Error");
+	echo (json_encode($arraytoreturn));
 } else {
+	// Otherwise return 200 and the output.
 	echo (json_encode($arraytoreturn));
 }
 ?>
