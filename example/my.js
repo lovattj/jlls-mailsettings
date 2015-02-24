2b5955ed044958860526754cee9dd06f259487a3
Jonathan <lovattj@me.com>
2015-02-24 19:44:04 +0000

    Added new functions to example
    E-Mail address search, so you can type in an e-mail address and it pops up with the settings.

--- example/my.js
+++ example/my.js
@@ -15,16 +15,49 @@ $('body').on('hidden.bs.modal', '.modal', function() {
 
 $(document).on("click", "#btnListProviders", function () {
 	go('listProviders','null');
-	$("#listProviders-providers").html("<i class=\"fa fa-spinner fa-pulse\"></i> Loading...");
+	$("#listProviders-providers").html("<p class='loading-symbol'><i class=\"fa fa-spinner fa-pulse\"></i> Loading...</p>");
 	var providers = [];
 	$.when(getProviderList()).done(function(x) {
 			$.each(x, function(i, item) {
 				providers.push("<div class='col-xs-6 col-md-4'><a data-providerid=\""+item[0]+"\" class='thumbnail providerBox' href=\"#\">"+item[1]+"</a></div>");			
 			});
-			$("#listProviders-providers").empty();
+			$(".loading-symbol").remove();
 			$("#listProviders-providers").append(providers);
 			
 	});	
+});
+
+$(document).on("submit", "#emailsearchform", function() {
+	if ($('#emailsearch').val() === '') {
+		$('#emailsearch').focus();
+		$('#tag-search-form-group').addClass("has-error");
+	} else {
+		var emailaddr;
+		emailaddr = $("#emailsearch").val();
+		var split = emailaddr.split("@");
+		$("#providerincomingserverinformation").show();
+		$("#provideroutgoingserverinformation").show();
+		$("#providernotes").show();
+
+		$("#providerincomingserverinformation").append("<p class='loading-symbol'><i class=\"fa fa-spinner fa-pulse\"></i> Loading...</p>");
+		$("#provideroutgoingserverinformation").append("<p class='loading-symbol'><i class=\"fa fa-spinner fa-pulse\"></i> Loading...</p>");
+		$("#providernotes").append("<p class='loading-symbol'><i class=\"fa fa-spinner fa-pulse\"></i> Loading...</p>");
+		
+		$.when(getSettingsByEmailExtension(split[1]))
+			.done(function(x) {
+				displayProviderData(x);
+				$(".loading-symbol").remove();	
+				$('#tag-search-form-group').removeClass("has-error");
+			
+			})
+			.fail(function(e) {
+				$(".loading-symbol").remove();				
+				$('#tag-search-form-group').addClass("has-error");	
+				$('#emailsearch').focus();							
+			});
+	}
+	
+	return false;
 });
 
 $(document).on("click", ".providerBox", function() {
@@ -39,27 +72,7 @@ $(document).on("click", ".providerBox", function() {
 	$("#providernotes").append("<p class='loading-symbol'><i class=\"fa fa-spinner fa-pulse\"></i> Loading...</p>");
 	
 	$.when(getSettings(providerId)).done(function(x) {
-		document.title = x[0] + " | MailSettings";			
-	
-		var incomingHTML;
-		incomingHTML = "<b>Server Type: </b>" + x[1] + "<br />";
-		incomingHTML += "<b>Host Name: </b>" + x[2] + "<br />";
-	 	incomingHTML += "<b>Port: </b>" + x[3] + "<br />";
-		incomingHTML += "<b>SSL: </b>" + x[4] + "<br />";
-		$("#placeholder-incominginfo-text").html(incomingHTML);	
-
-		var outgoingHTML;
-		outgoingHTML = "<b>Server Type: </b>" + x[5] + "<br />";
-		outgoingHTML += "<b>Host Name: </b>" + x[6] + "<br />";
-	 	outgoingHTML += "<b>Port: </b>" + x[7] + "<br />";
-		outgoingHTML += "<b>SSL: </b>" + x[8] + "<br />";
-		$("#placeholder-outgoinginfo-text").html(outgoingHTML);		
-
-		var authNotes;
-		authNotes = "<b>Username: </b>" + x[10] + "<br />";
-		authNotes += "<br /><p><b>Notes:</b> " + x[9] + "</p>";
-		$("#placeholder-notes-text").html(authNotes);
-		
+		displayProviderData(x);
 		$(".loading-symbol").remove();	
 		
 	});
@@ -93,21 +106,63 @@ function go(where, opt) {
 	
 }
 
+function displayProviderData(x) {
+		document.title = x[0] + " | MailSettings";			
+	
+		var incomingHTML;
+		incomingHTML = "<b>Server Type: </b>" + x[1] + "<br />";
+		incomingHTML += "<b>Host Name: </b>" + x[2] + "<br />";
+	 	incomingHTML += "<b>Port: </b>" + x[3] + "<br />";
+		incomingHTML += "<b>SSL: </b>" + x[4] + "<br />";
+		$("#placeholder-incominginfo-text").html(incomingHTML);	
+
+		var outgoingHTML;
+		outgoingHTML = "<b>Server Type: </b>" + x[5] + "<br />";
+		outgoingHTML += "<b>Host Name: </b>" + x[6] + "<br />";
+	 	outgoingHTML += "<b>Port: </b>" + x[7] + "<br />";
+		outgoingHTML += "<b>SSL: </b>" + x[8] + "<br />";
+		$("#placeholder-outgoinginfo-text").html(outgoingHTML);		
+
+		var authNotes;
+		authNotes = "<b>Username: </b>" + x[10] + "<br />";
+		authNotes += "<br /><p><b>Notes:</b> " + x[9] + "</p>";
+		$("#placeholder-notes-text").html(authNotes);
+}
+
 // Methods...	
 
 function hello() {
 	alert("(c)2015, Jonathan Lovatt.");
 }
 
-function validateform() {
-	if ($('#emailsearch').val() === '') {
-		$('#emailsearch').focus();
-		$('#tag-search-form-group').addClass("has-error");
-	} else {
-		var emailaddr;
-		emailaddr = $("#emailsearch").val();
-		alert(emailaddr);
-	}
+
+function getSettingsByEmailExtension(ext) {
+	var settingsArray = [];
+	return $.Deferred(function() {
+		var self = this;
+		$.getJSON( MAILSETTINGS_BASE_API(), {
+			method: "getproviderbyextension",
+			extension: ext
+		})
+		
+		.done(function(data) {
+			settingsArray.push(data[0].providername);
+			settingsArray.push(data[0].incomingtype);
+			settingsArray.push(data[0].incomingserver);
+			settingsArray.push(data[0].incomingport);
+			settingsArray.push(data[0].incomingssl);
+			settingsArray.push(data[0].outgoingtype);
+			settingsArray.push(data[0].outgoingserver);
+			settingsArray.push(data[0].outgoingport);
+			settingsArray.push(data[0].outgoingssl);
+			settingsArray.push(data[0].notes);
+			settingsArray.push(data[0].username);
+			self.resolve(settingsArray);
+		})
+		.fail(function(jqxhr, textstatus, error) {
+			self.reject(error);
+		});
+	});
 }
 
 function getSettings(providerid) {
@@ -132,6 +187,9 @@ function getSettings(providerid) {
 			settingsArray.push(data[0].notes);
 			settingsArray.push(data[0].username);
 			self.resolve(settingsArray);
+		})
+		.fail(function(jqxhr, textstatus, error) {
+			self.reject(error);
 		});
 	});
 }
